af2b5956d6c08035c65b11db843d71d07c319253
MaksimFridrih <maximus5610@mail.ru>
2022-01-26 19:35:20 +0300

    impl add(int,int)

--- Overone 101/src/home/task/Lessons10.java
+++ Overone 101/src/home/task/Lessons10.java
@@ -1,4 +1,12 @@
 package home.task;
 
 public class Lessons10 {
+    public static void main(String[] args) {
+        System.out.println(add(5, 4));
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
